8b72932e9362517005256c8c2a4e60d9a882e72c
Yakimov Ivan <legendthechaos@gmail.com>
2023-10-31 20:38:14 +0600

    Update RabinKarp hashing algorithm
    Updated the RabinKarp.cpp to optimize the string hashing method. Implemented a new method for calculating the hash of the next substring in the text. The change reduces the time complexity by avoiding needless rehashing for each substring. Also, removed a debug line which printed the original text and pattern.

--- Hashing/RabinKarp.cpp
+++ Hashing/RabinKarp.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <cmath>
 
 using namespace std;
 
 int base = 31;
 int mod = 1e9 + 7;
 
-int getHash(string& s){
+int getHash(string s){
 
     int result = 0;
+    int length = s.length();
 
-    for(char c : s){
-        result += c - 'a' + 1;
+    for(int i = length -1; i >= 0; i++){
+        int charHash = s[i] - 'a' + 1;
         result *= base;
+        result += charHash;
         result %= mod;
     }
 
     return result;
 }
+
+int simplePower(int a, int b){
+
+        int result = 1;
+
+        for(int i = 0; i < b; i++){
+            result *= a;
+        }
+
+        return result;
+}
+
+int getNextHash(int prevHash, char newChar, int length){
+
+    int result = prevHash/base;
+    int newCharHash = newChar - 'a' + 1;
+    int power = simplePower(base, length - 1) * newCharHash;
+    result += power;
+
+    return result;
+}
+
+
+
 
 bool nativeCompare(string s1, string s2){
 
@@ -39,17 +65,16 @@ vector<int> rabinKarp(string& text, string& pattern){
     vector<int> result;
 
     int patternHash = getHash(pattern);
-    unsigned int patternLength = pattern.length();
 
-    for(int i = 0; i <= text.length() - pattern.length(); i++){
+    int textHash = getHash(text.substr(0, pattern.length()));
 
-        string sub = text.substr(i, patternLength);
 
-        int subHash = getHash(sub);
-
-        if(subHash == patternHash){
-            if(nativeCompare(pattern, sub)) result.push_back(i);
+    for(int i = 0; i < text.length() - pattern.length()+1; i++){
+        if(patternHash == textHash){
+            result.push_back(i);
         }
+
+        textHash = getNextHash(textHash, text[i + pattern.length()], pattern.length());
     }
 
     return result;
@@ -70,8 +95,6 @@ int main(){
     string text, pattern;
 
     cin >> text >> pattern;
-
-    cout << text << " # " << pattern << endl;
 
     vector<int> result = rabinKarp(text, pattern);
 
